1f176f69372e96ea0f06b2f9216ef0211e0afa89
DreamFollowersAM <90816138+DreamFollowersAM@users.noreply.github.com>
2021-11-01 00:12:54 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -69,6 +69,9 @@ const p = new Vue({
     busc (){
       return this.arrcffe.filter(o => o.nm.toUpperCase().includes(w.fnm.toUpperCase().trim())).filter(o => o.pr <= w.fpm);
     },
+    cnm () {
+      return w.fnm;
+    }
   }
  
 });
